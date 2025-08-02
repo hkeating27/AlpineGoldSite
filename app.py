326731d6446994b1f675652f3836aaea5d2458aa
Hunter Keating <hkeating27@gmail.com>
2025-08-02 14:33:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -161,11 +161,12 @@ def create_app():
 
     return app
 
+app = create_app()
 
 if __name__ == '__main__':
-    application = create_app()
     port = int(os.environ.get('PORT', 5000))
     application.run(host='0.0.0.0', port=port)
+
 
 
 
